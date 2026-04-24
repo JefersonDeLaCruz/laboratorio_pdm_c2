36d7692a41429d783ff1bdfab4bbb230da975de3
manuelmv15 <mv23036@ues.edu.sv>
2026-04-23 18:45:16 -0600

    feat: Dao articulo

---
+++ app/src/main/java/com/example/laboratorio_pdm_c2/Dao/ArticuloDao.java
@@ -0,0 +1,32 @@
+package com.example.laboratorio_pdm_c2.Dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.laboratorio_pdm_c2.Entitys.Articulo;
+
+import java.util.List;
+
+@Dao
+public interface ArticuloDao {
+
+    @Insert
+    long insertArticulo(Articulo articulo);
+
+    @Query("SELECT * FROM articulos")
+    List<Articulo> getAllArticulos();
+
+    @Query("SELECT * FROM articulos WHERE idarticulo = :idarticulo")
+    Articulo getArticulo(int idarticulo);
+
+    @Update
+    long updateArticulo(Articulo articulo);
+
+    @Delete
+    long deleteArticulo(Articulo articulo);
+
+
+}
