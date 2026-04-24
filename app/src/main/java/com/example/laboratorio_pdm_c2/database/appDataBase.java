0f6586a75c77684a4089e8ddf0106ea7ccf37981
manuelmv15 <mv23036@ues.edu.sv>
2026-04-23 19:13:01 -0600

    feat-db: appDatabase

---
+++ app/src/main/java/com/example/laboratorio_pdm_c2/database/appDataBase.java
@@ -0,0 +1,45 @@
+package com.example.laboratorio_pdm_c2.database;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.laboratorio_pdm_c2.Dao.CategoriaDao;
+import com.example.laboratorio_pdm_c2.Entitys.Articulo;
+import com.example.laboratorio_pdm_c2.Entitys.Categoria;
+import com.example.laboratorio_pdm_c2.Entitys.Persona;
+import com.example.laboratorio_pdm_c2.Entitys.Prestamo;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import kotlin.jvm.Synchronized;
+
+@Database(entities = {Categoria.class, Articulo.class, Persona.class, Prestamo.class}, version = 1,exportSchema = true)
+public abstract class appDataBase  extends RoomDatabase {
+
+    public abstract CategoriaDao categoriaDao();
+
+    private static volatile appDataBase INSTANCE;
+
+    public static final ExecutorService databaseWriteExcecutor = Executors.newFixedThreadPool(4);
+
+    public  static appDataBase getINSTANCE(Context context) {
+
+        if (INSTANCE == null) {
+            synchronized (appDataBase.class) {
+                INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        appDataBase.class,
+                        "nexus_db"
+                ).build();
+            }
+        }
+
+        return INSTANCE;
+    }
+}
