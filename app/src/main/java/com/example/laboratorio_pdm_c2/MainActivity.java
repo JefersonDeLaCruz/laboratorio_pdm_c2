bbc5e97310981292598c6f521fbc598243b49fc8
manuelmv15 <mv23036@ues.edu.sv>
2026-04-24 07:41:20 -0600

    feat: vista, creacion de categoria y articulos

--- app/src/main/java/com/example/laboratorio_pdm_c2/MainActivity.java
+++ app/src/main/java/com/example/laboratorio_pdm_c2/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.laboratorio_pdm_c2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -10,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -17,7 +17,6 @@ import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView navbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,37 +31,45 @@ public class MainActivity extends AppCompatActivity {
 
         setupViews();
         setupEvents();
+
+        // Cargar el fragmento por defecto (Artículos)
+        if (savedInstanceState == null) {
+            loadFragment(new ArticulosFragment());
+        }
     }
-
-
 
     private void setupViews(){
-
         navbar = findViewById(R.id.nav_bar);
-
     }
-
 
     private void setupEvents(){
         navbar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
+                Fragment selectedFragment = null;
                 int id = menuItem.getItemId();
 
                 if (id == R.id.item_1) {
-                    Toast.makeText(MainActivity.this, "Artículos seleccionado", Toast.LENGTH_SHORT).show();
-                    return true;
+                    selectedFragment = new ArticulosFragment();
                 } else if (id == R.id.item_2) {
-                    Toast.makeText(MainActivity.this, "Personas seleccionado", Toast.LENGTH_SHORT).show();
-                    return true;
+                    // Placeholder para PersonasFragment
                 } else if (id == R.id.item_3) {
-                    Toast.makeText(MainActivity.this, "Préstamos seleccionado", Toast.LENGTH_SHORT).show();
-                    return true;
+                    // Placeholder para PrestamosFragment
                 }
 
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                    return true;
+                }
                 return false;
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
