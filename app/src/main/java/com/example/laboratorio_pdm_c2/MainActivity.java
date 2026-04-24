4bea5486e04a634c0a11c301e67aa6231a6bf9f5
manuelmv15 <mv23036@ues.edu.sv>
2026-04-23 19:51:44 -0600

    feat-db: prueba de insert en la db

--- app/src/main/java/com/example/laboratorio_pdm_c2/MainActivity.java
+++ app/src/main/java/com/example/laboratorio_pdm_c2/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.laboratorio_pdm_c2;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.laboratorio_pdm_c2.Entitys.Articulo;
+import com.example.laboratorio_pdm_c2.Entitys.Categoria;
+import com.example.laboratorio_pdm_c2.database.appDataBase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        // 1. Obtener la instancia de la DB
+        appDataBase db = appDataBase.getINSTANCE(this);
+
+        // 2. Usar el executor que definiste en tu clase appDataBase
+        appDataBase.databaseWriteExcecutor.execute(() -> {
+            try {
+                // --- PRUEBA DE CATEGORÍA ---
+                Categoria cat = new Categoria();
+                cat.nombre = "Electrónica";
+                db.categoriaDao().insertCategoria(cat);
+
+                // --- PRUEBA DE ARTÍCULO ---
+                // Nota: Asegúrate de que el id de la categoría coincida
+                // o que tu objeto Articulo esté bien configurado.
+                Articulo art = new Articulo();
+                art.nombre = "Cargador Tipo C";
+                art.descripcion = "Cargador rápido 25W";
+                art.idcategoria = 1; // Asumiendo que el ID generado fue 1
+
+                db.articuloDao().insertArticulo(art);
+
+                // --- CONSULTAR PARA VERIFICAR ---
+                int totalArticulos = db.articuloDao().getAllArticulos().size();
+
+                // Ver el resultado en el Logcat (Filtra por "NexusDB")
+                Log.d("NexusDB", "Prueba exitosa. Artículos en DB: " + totalArticulos);
+
+            } catch (Exception e) {
+                Log.e("NexusDB", "Error en la base de datos: " + e.getMessage());
+            }
+        });
+
     }
 }
